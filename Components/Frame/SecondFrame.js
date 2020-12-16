2c4e44bb91d85f697ce75d68e5dcfb10bff1a6bc
shlee <kya100789@naver.com>
2020-12-16 20:53:12 +0900

    default case

--- Components/Frame/SecondFrame.js
+++ Components/Frame/SecondFrame.js
@@ -1,2 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+
+export default class HomeTab extends Component {
+    static navigationOptions = {
+        tabBarIcon: ({ tintColor }) => (
+            <AntDesign name="home" size={24} style={{ color: tintColor }} />
+        )
+    }
+
+    render() {
+        return (
+            <View style={style.container}>
+                <Text>SecondTab</Text>
+            </View>
+        );
+    }
+}
+ 
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation-tabs'; 
+import { Platform , StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createMaterialTopTabNavigator} from 'react-navigation-tabs'; 
 import FirstFrame from './Frame/FirstFrame'
 import SecondFrame from './Frame/SecondFrame'
 import thirdFrame from './Frame/thirdFrame'
 
-// 하단 탭 네비게이터 생성
-const AppTabNavigator = createBottomTabNavigator({
-  HomeTab: { screen: HomeTab },
-  SearchTab: { screen: SearchTab },
-  AddMediaTab: { screen: AddMediaTab },
-  LikesTab: { screen: LikesTab },
-  ProfileTab: { screen: ProfileTab }
+const AppTabNavigator = createMaterialTopTabNavigator({
+  FirstFrame: { screen: FirstFrame },
+  SecondFrame: { screen: SecondFrame },
+  thirdFrame: { screen: thirdFrame }
+}, {
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        ios:{
+          backgroundColor:'white',
+        },
+        android: {
+          backgroundColor:'white',
+        },
+        web: {
+          backgroundColor:'white',
+        }
+      })
+    },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: false,
+    showLabel: false,
+    showIcon: true,
+  }
 });
+
 
 const AppTabContainet = createAppContainer(AppTabNavigator);
 
 export default class MainScreen extends Component {
   
- // navigationOptions 코드 생략
-
+  static navigationOptions = {
+    headerShown: false
+  }
   render() {
     return <AppTabContainet/>; // AppTabContainet 컴포넌트를 리턴한다.
   }
